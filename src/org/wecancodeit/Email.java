2da4a76945208847e3ce3fbe6d31c5cc3701fde6
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 10:35:37 -0400

    added setDepartment method

--- src/org/wecancodeit/Email.java
+++ src/org/wecancodeit/Email.java
@@ -1,5 +1,7 @@
 package org.wecancodeit;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -24,5 +26,23 @@ public class Email {
 
         email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + compaySuffix;
 
+    }
+
+    public String setDepartment() {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter your department code: \n1:Accounting\n2:Sales\n3:Admin\n4:Dev");
+        int deptChoice = scn.nextInt();
+
+        if (deptChoice == 1) {
+            return "Accounting";
+        } else if (deptChoice == 2) {
+            return "Sales";
+        } else if (deptChoice == 3) {
+            return "Admin";
+        } else if (deptChoice == 4) {
+            return "Dev";
+        } else {
+            return "";
+        }
     }
 }
