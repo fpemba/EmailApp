ebade85a89bd399c3857c4abd963de009e4bbcde
fpemba <fatuma.pemba@yahoo.com>
2020-10-04 10:36:32 -0400

    added randomPassword method

--- src/org/wecancodeit/Email.java
+++ src/org/wecancodeit/Email.java
@@ -45,4 +45,16 @@ public class Email {
             return "";
         }
     }
+
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIGKLMOPQRSTUVWXYZ0123456789!@#$";
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            int random = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(random);
+        }
+
+        return new String(password);
+    }
 }
